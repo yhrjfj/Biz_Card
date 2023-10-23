1f9d9c60a3664c29ff2b4b8a56458f4eb3b6cd2c
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-23 11:58:14 +0600

    Card

--- app/src/main/java/com/yhr/jfj/biz_card/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/biz_card/MainActivity.kt
@@ -3,13 +3,25 @@ package com.yhr.jfj.biz_card
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.yhr.jfj.biz_card.ui.theme.Biz_CardTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +42,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CreateBizCard(){
+fun CreateBizCard() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+    ) {
+        Card(
+            elevation = CardDefaults.cardElevation(5.dp),
+            modifier = Modifier
+                .padding(16.dp)
+                .border(1.5.dp, Color.White, shape = RoundedCornerShape(10.dp)),
+            colors = CardDefaults.cardColors(Color.White)
+        ) {
 
+        }
+    }
 }
 
 @Preview(showBackground = true)
