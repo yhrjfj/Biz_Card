80dfe539d987c718cf50d47e73ba74875441443f
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-23 13:35:33 +0600

    Add Image

--- app/src/main/java/com/yhr/jfj/biz_card/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/biz_card/MainActivity.kt
@@ -3,6 +3,8 @@ package com.yhr.jfj.biz_card
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -10,7 +12,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -20,6 +24,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yhr.jfj.biz_card.ui.theme.Biz_CardTheme
@@ -50,12 +56,28 @@ fun CreateBizCard() {
     ) {
         Card(
             elevation = CardDefaults.cardElevation(5.dp),
-            modifier = Modifier.width(200.dp).height(390.dp)
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
                 .padding(16.dp)
                 .border(1.5.dp, Color.White, shape = RoundedCornerShape(10.dp)),
             colors = CardDefaults.cardColors(Color.White)
         ) {
-
+            Surface(
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(5.dp),
+                shape = CircleShape,
+                border = BorderStroke(0.5.dp, Color.LightGray),
+                shadowElevation = 4.dp,
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.main),
+                    contentDescription = "Picture",
+                    contentScale = ContentScale.Crop
+                )
+            }
         }
     }
 }
